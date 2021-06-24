913c3bb764ea0ae23ecc056d6dce1a36894beca9
Siarhei Ladzeika <sergey.ladeiko@bpmobile.com>
2021-06-24 17:58:25 +0300

    bump version to 2.2.0

--- lib/viperaptor/version.rb
+++ lib/viperaptor/version.rb
@@ -1,5 +1,5 @@
 module Viperaptor
-  VERSION = '2.1.0'
-  RELEASE_DATE = '01.02.2021'
+  VERSION = "2.2.0"
+  RELEASE_DATE = "24.06.2021"
   RELEASE_LINK = "https://github.com/ladeiko/Viperaptor/releases/tag/#{VERSION}"
 end
